d4b477182d0762f600c9eea3e287d81c7d0b9577
Kevin R. Thornton <krthornt@uci.edu>
2025-02-10 11:54:00 -0800

    derp

--- stats_play.py
+++ stats_play.py
@@ -60,7 +60,6 @@ def make_allele_count_list(ts: tskit.TreeSequence):
                 node = ts.mutation(mut_index).node
                 temp = mut_at_site
                 # WARNING: the bits below are possibly broken
-                num_samples_with_derived_state[parent[node]] += 1
                 nd = num_samples_below[node] - num_samples_with_derived_state[node]
                 if nd > 0:
                     try:
@@ -80,6 +79,8 @@ def make_allele_count_list(ts: tskit.TreeSequence):
                                 mut_allele}"
                         )
                         allele_counts.append(0)
+                    num_samples_with_derived_state[parent[node]] += 1
+                    print(mut_allele, nd)
                     allele_counts[mut_allele] += nd
                     allele_counts[0] -= nd
                 temp += 1
@@ -281,9 +282,16 @@ def test_case_5():
     n3 = tables.nodes.add_row(tskit.NODE_IS_SAMPLE, time=0.0)
     n4 = tables.nodes.add_row(0, time=1.0)
     n5 = tables.nodes.add_row(0, time=2.0)
-    n5 = tables.nodes.add_row(0, time=3.0)
+    n6 = tables.nodes.add_row(0, time=3.0)
 
     s0 = tables.sites.add_row(5.0, ancestral_state="G")
+
+    tables.edges.add_row(0, 10, n6, n5)
+    tables.edges.add_row(0, 10, n6, n3)
+    tables.edges.add_row(0, 10, n5, n4)
+    tables.edges.add_row(0, 10, n5, n2)
+    tables.edges.add_row(0, 10, n4, n1)
+    tables.edges.add_row(0, 10, n4, n0)
 
     m0 = tables.mutations.add_row(s0, node=5, time=2.1, derived_state="A")
     m1 = tables.mutations.add_row(s0, node=4, time=1.1, derived_state="G")
@@ -291,6 +299,8 @@ def test_case_5():
 
     tables.sort()
     ts = tables.tree_sequence()
+    print(ts.draw_text())
+    print(tables.mutations)
     allele_counts = make_allele_count_list(ts)
     assert len(allele_counts) == 1
     assert allele_counts[0] == [2, 1, 1]
